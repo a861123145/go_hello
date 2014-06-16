7b086ba5b708ee327ac07b3c1cce458901ac7591
David.Zhang <David.DW.Zhang@wnc.com.tw>
2014-06-16 15:55:58 +0800

    hello.

---
+++ hello.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/a86112145/newmath"
+)
+
+func main() {
+	fmt.Printf("Hello, world.\n")
+	fmt.Printf("hello, world. Sqrt(2) = %v\n", newmath.Sqrt(2))
+}
